213706edf906d8ee023fc5684567842b69eb6cff
Trang Le <lethutrang101@gmail.com>
2020-10-20 16:48:16 +0700

    Configuration settings for storing client id and secret

--- bin/godri.js
+++ bin/godri.js
@@ -1,2 +1,10 @@
 #! /usr/bin/env node
-console.log("Hello Otto");
+
+const CredentialManager = require('../lib/credentialManager');
+
+async function main() {
+  const creds = new CredentialManager('gogol');
+  const [id, secret] = await creds.getIdAndSecret();
+  console.log(id, secret);
+}
+main().catch(console.error);
